3cf18e6edf202b466a6554f59460a27bde0320ae
peterhchen <peter.hp.chen@gmail.com>
2020-08-23 16:03:07 -0700

    Add files via upload

---
+++ 064_routing/movie-rater/src/app/main/main.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MainComponent } from './main.component';
+
+const routes: Routes = [
+  {path: 'movies', component: MainComponent }
+];
+
+@NgModule({
+  declarations: [MainComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild (routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class MainModule { }
