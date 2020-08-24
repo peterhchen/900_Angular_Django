a631a54f838682ec95927eec366a0212f496ece6
peterhchen <peter.hp.chen@gmail.com>
2020-08-23 20:13:26 -0700

    Add files via upload

---
+++ 065_main-comp/movie-rater/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AuthModule } from './auth/auth.module';
+import { MainModule } from './main/main.module';
+
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'movies'}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    MainModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
